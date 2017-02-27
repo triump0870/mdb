0d371435ef3dac87688b92998e8f8b030db4f85a
Rohan Roy <b4you0870@gmail.com>
2017-02-27 15:48:36 +0530

    fabfile is now following PEP8

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,15 @@
 from fabric.api import local
-import os,sys
+import os, sys
+
 
 def manage():
     # command = raw_input("Enter manage command:")
-    local('./src/manage.py %s'%sys.argv[2])
+    local('./src/manage.py %s' % sys.argv[2])
+
 
 def untrack():
     local('git add -A')
+
 
 def migrate():
     local('heroku maintenance:on')
@@ -18,26 +21,31 @@ def migrate():
 def git():
     local('git pull origin master')
 
+
 def heroku():
     local('heroku maintenance:on')
     local('git push -f heroku master')
     local('heroku maintenance:off')
 
+
 def deploy():
     local('git add .')
-    print "Enter the git commit comment: "
+    print
+    "Enter the git commit comment: "
     comment = raw_input()
-    local(' git commit -m "%s"'%comment)
+    local(' git commit -m "%s"' % comment)
     local('git push origin master')
     local('heroku maintenance:on')
     local('git push heroku master')
     local('heroku maintenance:off')
 
+
 def runserver():
     local('python src/manage.py runserver 0.0.0.0:8000')
 
+
 def dump():
-    from os.path import join,dirname,exists
+    from os.path import join, dirname, exists
     import environ
     env = environ.Env()
 
@@ -50,19 +58,22 @@ def dump():
     USERNAME = env('DB_USER')
     PASSWORD = env('DB_PASS')
 
-    local('PGPASSWORD={0} pg_dump -Fc --no-acl --no-owner -h localhost -U {1} {2}  > {2}.dump'.format(PASSWORD,USERNAME,DATABASE_NAME))
-    local("heroku config:set AWS_STORAGE_BUCKET_NAME=%s"%env('AWS_STORAGE_BUCKET_NAME'))
-    local("heroku config:set AWS_ACCESS_KEY=%s"%os.environ['AWS_ACCESS_KEY'])
-    local("heroku config:set AWS_SECRET_ACCESS_KEY=%s"%os.environ['AWS_SECRET_ACCESS_KEY'])
+    local(
+        'PGPASSWORD={0} pg_dump -Fc --no-acl --no-owner -h localhost -U {1} {2}  > {2}.dump'.format(PASSWORD, USERNAME,
+                                                                                                    DATABASE_NAME))
+    local("heroku config:set AWS_STORAGE_BUCKET_NAME=%s" % env('AWS_STORAGE_BUCKET_NAME'))
+    local("heroku config:set AWS_ACCESS_KEY=%s" % os.environ['AWS_ACCESS_KEY'])
+    local("heroku config:set AWS_SECRET_ACCESS_KEY=%s" % os.environ['AWS_SECRET_ACCESS_KEY'])
     S3_URL = 'https://{}.s3.amazonaws.com/{}.dump'.format(env('AWS_STORAGE_BUCKET_NAME'), DATABASE_NAME)
-#    local('aws configure')
-    S3_UPLOAD_URL = 's3://%s/'%(os.environ['AWS_STORAGE_BUCKET_NAME'])
-    local('rm %s.dump'%DATABASE_NAME)
-    local('aws s3 cp %s.dump %s'%(DATABASE_NAME, S3_UPLOAD_URL))
-    local("heroku pg:backups restore '%s' DATABASE_URL --confirm movie-task"%(S3_URL))
+    #    local('aws configure')
+    S3_UPLOAD_URL = 's3://%s/' % (os.environ['AWS_STORAGE_BUCKET_NAME'])
+    local('rm %s.dump' % DATABASE_NAME)
+    local('aws s3 cp %s.dump %s' % (DATABASE_NAME, S3_UPLOAD_URL))
+    local("heroku pg:backups restore '%s' DATABASE_URL --confirm movie-task" % (S3_URL))
+
 
 def aws():
     local('git add .')
     comment = raw_input("Enter the commit comment: ")
-    local('git commit -m "%s"'%comment)
+    local('git commit -m "%s"' % comment)
     local('git push origin master')
